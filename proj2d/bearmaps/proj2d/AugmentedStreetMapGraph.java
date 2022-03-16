8c63b699384ec762fa97736837403ebc2846c0b8
tuysss <2862670249@qq.com>
2022-03-16 21:21:34 +0800

    finish proj2d Part3: Autocomplete & Search

--- proj2d/bearmaps/proj2d/AugmentedStreetMapGraph.java
+++ proj2d/bearmaps/proj2d/AugmentedStreetMapGraph.java
@@ -1,14 +1,12 @@
 package bearmaps.proj2d;
 
+import bearmaps.lab9.MyTrieSet;
 import bearmaps.proj2ab.Point;
 import bearmaps.proj2c.streetmap.StreetMapGraph;
 import bearmaps.proj2c.streetmap.Node;
 import bearmaps.proj2ab.KDTree;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * An augmented graph that is more powerful than a standard StreetMapGraph.
@@ -21,7 +19,14 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
     private Map<Point,Long> pointToID;
     private KDTree kdTree ;
 
+    private MyTrieSet trieSet;
+    private Map<String,List<Node>> cleanedNameToNodes;
 
+    /**
+     * build 2 trees in this constructor:
+     * TrieSet : to autocomplete with prefix
+     * KDTree : to find the closest path
+     */
     public AugmentedStreetMapGraph(String dbPath) {
         super(dbPath);
         // You might find it helpful to uncomment the line below:
@@ -30,7 +35,27 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
         List<Point> points=new LinkedList<>();
         List<Node> nodes=this.getNodes();
 
+        trieSet=new MyTrieSet();
+        cleanedNameToNodes=new HashMap<>();
+        List<Node> nodesList;
+
         for(Node node:nodes){
+            //if the node has a name, clean it, then add it to the tire tree
+            //and put the <cleaned name, list of nodes> pair into hashmap
+            if(node.name()!=null){
+                String cleaned=cleanString(node.name());
+                trieSet.add(cleaned);
+
+                if(!cleanedNameToNodes.containsKey(cleaned)){
+                    cleanedNameToNodes.put(cleaned,new LinkedList<>());
+                }
+                nodesList=cleanedNameToNodes.get(cleaned);
+                nodesList.add(node);
+                cleanedNameToNodes.put(cleaned,nodesList);
+            }
+
+            //only consider the nodes that have neighbors(which is a road ,not a place)
+            //turn these <Node>s into <Point>s to service KDTree
             long id=node.id();
             if(!this.neighbors(id).isEmpty()){
                 double x=node.lon();
@@ -68,7 +93,17 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * cleaned <code>prefix</code>.
      */
     public List<String> getLocationsByPrefix(String prefix) {
-        return new LinkedList<>();
+        String cleanedPrefix=cleanString(prefix);
+        List<String> matching=trieSet.keysWithPrefix(cleanedPrefix);
+        Set<String> locations=new HashSet<>();
+
+        for(String name: matching){
+            for(Node node: cleanedNameToNodes.get(name)){
+                locations.add(node.name());
+            }
+        }
+
+        return new LinkedList<>(locations);
     }
 
     /**
@@ -85,7 +120,21 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * "id" -> Number, The id of the node. <br>
      */
     public List<Map<String, Object>> getLocations(String locationName) {
-        return new LinkedList<>();
+        List<Map<String, Object>> locations=new LinkedList<>();
+        String cleaned=cleanString(locationName);
+
+        if(cleanedNameToNodes.containsKey(cleaned)){
+            for(Node node: cleanedNameToNodes.get(cleaned)){
+                Map<String,Object> locInfo=new HashMap<>();
+                locInfo.put("lat",node.lat());
+                locInfo.put("lon",node.lon());
+                locInfo.put("name",node.name());
+                locInfo.put("id",node.id());
+                locations.add(locInfo);
+            }
+        }
+
+        return locations;
     }
 
 
